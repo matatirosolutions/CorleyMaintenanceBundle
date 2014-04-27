83f9553cb1627b5af5051e614e0c5809a4868cde
Walter Dal Mut <walter.dalmut@gmail.com>
2014-04-27 10:09:58 +0200

    Added extension test

---
+++ Tests/DependencyInjection/CorleyMaintenanceExtensionTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Corley\MaintenanceBundle\Tests\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Corley\MaintenanceBundle\DependencyInjection\CorleyMaintenanceExtension;
+
+class CorleyMaintenanceExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    private $container;
+
+    private $symlink;
+    private $copy;
+
+    public function setUp()
+    {
+        $this->kernel = $this->getMock('Symfony\\Component\\HttpKernel\\KernelInterface');
+
+        $this->symlink = $this->getMockBuilder('Corley\MaintenanceBundle\Maintenance\Strategy\SymlinkStrategy')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->copy = $this->getMockBuilder('Corley\MaintenanceBundle\Maintenance\Strategy\CopyStrategy')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->container = new ContainerBuilder();
+
+        $this->container->setParameter('kernel.root_dir', __DIR__);
+        $this->container->set('kernel', $this->kernel);
+
+        $this->container->set('corley_maintenance.strategy.symlink', $this->symlink);
+        $this->container->set('corley_maintenance.strategy.copy', $this->copy);
+    }
+
+    /**
+     * @dataProvider symlinkModes
+     */
+    public function testNotSymlink($config, $class)
+    {
+        $extension = new CorleyMaintenanceExtension();
+        $extension->load($config, $this->container);
+
+        $this->assertInstanceOf($class, $this->container->get('corley_maintenance.strategy'));
+    }
+
+    public function symlinkModes()
+    {
+        return array(
+            array(array(array('symlink' => false)), 'Corley\MaintenanceBundle\Maintenance\Strategy\CopyStrategy'),
+            array(array(array('symlink' => true)), 'Corley\MaintenanceBundle\Maintenance\Strategy\SymlinkStrategy'),
+        );
+    }
+}
